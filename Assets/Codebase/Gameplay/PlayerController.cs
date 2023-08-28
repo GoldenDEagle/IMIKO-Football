b5bed48c9106ca9d15f9cdd72153193900cba431
GoldenDEagle <ionufriev95@gmail.com>
2023-08-28 23:37:18 +0600

    controls

---
+++ Assets/Codebase/Gameplay/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Assets.Codebase.Gameplay
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 2f;
+
+        private float _verticalDirection;
+        private float _horizontalDirection;
+        private Vector2 _direction;
+        private Rigidbody2D _rigidbody;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        public void SetVerticalDirection(float value)
+        {
+            _verticalDirection = value;
+        }
+
+        public void SetHorizontalDirection(float value)
+        {
+            _horizontalDirection = value;
+        }
+
+        private void FixedUpdate()
+        {
+            _direction = new Vector2(_horizontalDirection, _verticalDirection);
+            _rigidbody.velocity = _direction * _speed;
+        }
+    }
+}
---
+++ Assets/Codebase/Gameplay/PlayerInputReader.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.Codebase.Gameplay
+{
+    [RequireComponent(typeof(PlayerController))]
+    public class PlayerInputReader : MonoBehaviour
+    {
+        private PlayerController _playerController;
+
+        private void Awake()
+        {
+            _playerController = GetComponent<PlayerController>();
+        }
+
+        public void OnUpPressed(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                _playerController.SetVerticalDirection(1f);
+            }
+
+            if (context.canceled)
+            {
+                _playerController.SetVerticalDirection(0f);
+            }
+        }
+
+        public void OnDownPressed(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                _playerController.SetVerticalDirection(-1f);
+            }
+
+            if (context.canceled)
+            {
+                _playerController.SetVerticalDirection(0f);
+            }
+        }
+
+        public void OnLeftPressed(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                _playerController.SetHorizontalDirection(-1f);
+            }
+
+            if (context.canceled)
+            {
+                _playerController.SetHorizontalDirection(0f);
+            }
+        }
+
+        public void OnRightPressed(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                _playerController.SetHorizontalDirection(1f);
+            }
+
+            if (context.canceled)
+            {
+                _playerController.SetHorizontalDirection(0f);
+            }
+        }
+    }
+}
