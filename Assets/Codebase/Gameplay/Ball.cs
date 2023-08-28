02a6907d277614f05e73c5f5579021299d627e28
GoldenDEagle <ionufriev95@gmail.com>
2023-08-29 00:01:56 +0600

    basic gameplay

---
+++ Assets/Codebase/Gameplay/Ball.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Codebase.Gameplay
+{
+    public class Ball : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.GetComponent<PlayerController>())
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
