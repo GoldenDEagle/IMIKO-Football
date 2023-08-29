5debcfeb8fe5e2e334d5b0f9690ad324e60a979b
GoldenDEagle <ionufriev95@gmail.com>
2023-08-29 14:18:01 +0600

    concept of game flow

---
+++ Assets/Codebase/Gameplay/GameController.cs
@@ -0,0 +1,54 @@
+﻿using Assets.Codebase.Infrastructure.Services;
+using Assets.Codebase.Infrastructure.Services.GameStates;
+using Assets.Codebase.Infrastructure.Services.Progress;
+using Assets.Codebase.Infrastructure.Services.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Codebase.Gameplay
+{
+    public class GameController : MonoBehaviour
+    {
+        [SerializeField] private List<Map> _maps;
+
+        private IGameStateMachine _gameStates;
+        private IProgressService _progress;
+        private IUIFactory _ui;
+
+        private void Awake()
+        {
+            _gameStates = ServiceLocator.Container.Single<IGameStateMachine>();
+            _progress = ServiceLocator.Container.Single<IProgressService>();
+            _ui = ServiceLocator.Container.Single<IUIFactory>();
+        }
+
+        private void OnEnable()
+        {
+            _gameStates.OnAfterStateEnter += OnAfterGameStateChanged;
+        }
+
+        private void OnDisable()
+        {
+            _gameStates.OnAfterStateEnter -= OnAfterGameStateChanged;
+        }
+
+        private void OnAfterGameStateChanged(GameState newState)
+        {
+            if (newState == GameState.Game)
+            {
+                StartGame();
+            }
+        }
+
+        private void StartGame()
+        {
+            // Enable Hud
+
+            // Spawn Player
+
+            // Spawn Balls
+
+            // Start Timer
+        }
+    }
+}
---
+++ Assets/Codebase/Gameplay/Map.cs
@@ -0,0 +1,17 @@
+﻿using Assets.Codebase.Data;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Codebase.Gameplay
+{
+    public class Map : MonoBehaviour
+    {
+        [SerializeField] private MapId _id;
+        [SerializeField] private Transform _playerSpawnPosition;
+        [SerializeField] private List<Transform> _ballSpawnPositions;
+
+        public MapId Id => _id;
+        public Transform PlayerSpawnPosition => _playerSpawnPosition;
+        public List<Transform> BallSpawnPositions => _ballSpawnPositions;
+    }
+}
