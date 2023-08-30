03778666cee9069f40ef5fa757f48dd65cbac011
GoldenDEagle <ionufriev95@gmail.com>
2023-08-31 03:06:14 +0600

    leaderboard ui configuration

---
+++ Assets/Codebase/UI/LeaderboardItem.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Codebase.UI
+{
+    public class LeaderboardItem : MonoBehaviour
+    {
+        [SerializeField] private Image _placementImage;
+        [SerializeField] private TMP_Text _placement;
+        [SerializeField] private TMP_Text _name;
+        [SerializeField] private TMP_Text _score;
+
+        public void ConfigureItem()
+        {
+
+        }
+    }
+}
---
+++ Assets/Codebase/UI/Windows/LeaderboardWindow.cs
@@ -0,0 +1,31 @@
+﻿using Assets.Codebase.Infrastructure.Services;
+using Assets.Codebase.Infrastructure.Services.UI;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Codebase.UI.Windows
+{
+    public class LeaderboardWindow : BaseWindow
+    {
+        [SerializeField] private TMP_Text _header;
+        [SerializeField] private VerticalLayoutGroup _elementsGroup;
+
+        private IUIFactory _ui;
+
+        private void Awake()
+        {
+            _ui = ServiceLocator.Container.Single<IUIFactory>();
+        }
+
+        private void OnEnable()
+        {
+            _ui.HUD.OnBackPressed += CloseWindow;
+        }
+
+        private void OnDisable()
+        {
+            _ui.HUD.OnBackPressed -= CloseWindow;
+        }
+    }
+}
