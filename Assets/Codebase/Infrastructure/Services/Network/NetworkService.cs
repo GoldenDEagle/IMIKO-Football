2511e7c691ad61e087921dfae5c8310bcdbc5041
GoldenDEagle <ionufriev95@gmail.com>
2023-08-31 20:21:09 +0600

    minor improvements

--- Assets/Codebase/Infrastructure/Services/Network/NetworkService.cs
+++ Assets/Codebase/Infrastructure/Services/Network/NetworkService.cs
@@ -1,11 +1,7 @@
-﻿using System.Xml.Linq;
+﻿using HtmlAgilityPack;
 using System;
-using UnityEngine;
-using UnityEngine.Networking;
-using HtmlAgilityPack;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 
 namespace Assets.Codebase.Infrastructure.Services.Network
 {
@@ -14,36 +10,17 @@ namespace Assets.Codebase.Infrastructure.Services.Network
         private string _url = "https://aviabaffishpic.com/politic";
         private string _data;
 
-        private UnityWebRequest _request;
-
         public string GetPolicy()
         {
             return _data;
         }
-
-        //public void UpdatePolicy()
-        //{
-        //    if (_request != null) return;
-
-        //    _request = UnityWebRequest.Get(_url);
-        //    _request.SendWebRequest().completed += OnDataLoaded;
-        //}
-
-        //private void OnDataLoaded(AsyncOperation operation)
-        //{
-        //    if (operation.isDone)
-        //    {
-        //        _data = _request.downloadHandler.text;
-        //        Debug.Log(_data);
-        //        //ParseData(_data);
-        //    }
-        //}
 
         public void UpdatePolicy()
         {
             var web = new HtmlWeb();
             var doc = web.Load(_url);
             var html = doc.Text;
+
             ParseData(html);
         }
 
