cacdab44527ce59722f090f5ea9b24dd68c3dddb
GoldenDEagle <ionufriev95@gmail.com>
2023-08-31 18:01:08 +0600

    some parsing

--- Assets/Codebase/Infrastructure/Services/Network/NetworkService.cs
+++ Assets/Codebase/Infrastructure/Services/Network/NetworkService.cs
@@ -3,6 +3,8 @@ using System;
 using UnityEngine;
 using UnityEngine.Networking;
 using HtmlAgilityPack;
+using System.Linq;
+using System.Text;
 
 namespace Assets.Codebase.Infrastructure.Services.Network
 {
@@ -40,15 +42,63 @@ namespace Assets.Codebase.Infrastructure.Services.Network
         {
             var web = new HtmlWeb();
             var doc = web.Load(_url);
-            _data = doc.ParsedText;
+            var html = doc.Text;
+            ParseData(html);
+        }
+
+        private void ParseData(string html)
+        {
+            _data = GetTextFromHtml(html);
             Debug.Log(_data);
         }
 
-        private void ParseData(string data)
-        {
 
+
+        private static string GetTextFromHtml(string html)
+        {
+            if (string.IsNullOrEmpty(html))
+                return "";
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+            return GetTextFromNodes(htmlDoc.DocumentNode.ChildNodes);
         }
 
+        private static string GetTextFromNodes(HtmlNodeCollection nodes, int indent = 0)
+        {
+            StringBuilder texts = new StringBuilder();
+            string[] linebreaks = { "p", "br", "table", "th", "tr" };
+            string[] indentTag = { "ul", "li" };
+            foreach (var node in nodes)
+            {
+                if (node.Name.ToLowerInvariant() == "title")
+                    continue;
+                if (node.Name.ToLowerInvariant() == "style")
+                    continue;
+                if (node.HasChildNodes)
+                {
+                    if (indentTag.Contains(node.Name.ToLowerInvariant()))
+                        texts.Append(GetTextFromNodes(node.ChildNodes, indent + 1));
+                    else
+                        texts.Append(GetTextFromNodes(node.ChildNodes, indent));
+                }
+                else
+                {
+                    var innerText = node.InnerText;
+                    if (!string.IsNullOrWhiteSpace(innerText))
+                    {
+                        texts.Append(new String(' ', indent) + node.InnerText);
+                    }
+                }
 
+                if (node.Name.ToLowerInvariant() == "a")
+                    texts.Append("\r\n" + node.Attributes["href"].Value + "\r\n");
+                if (node.Name.ToLowerInvariant() == "img" && !node.Attributes["src"].Value.EndsWith("invis.gif"))
+                    texts.Append("\r\n" + node.Attributes["src"].Value + "\r\n");
+                if (linebreaks.Contains(node.Name.ToLowerInvariant()))
+                    texts.Append("\r\n");
+            }
+
+            return texts.ToString();
+        }
     }
 }
